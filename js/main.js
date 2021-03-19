529bd9a6b2a23f576beaf041f0029c8f5f0a6ab1
alex-rak <2304alex2304@gmail.com>
2021-03-19 11:24:42 +0500

    adaptiv fix

--- js/main.js
+++ js/main.js
@@ -4,10 +4,23 @@ window.onload = () => {
     for (var i = 1; i < classList.length; i++) {
       classList.remove(classList[i]);
     }
-    classList.add(`type-${id}`)
+    classList.add(`type-${id}-before`);
+    classList.add(`type-${id}-after`);
+    setTimeout(() => {
+      for (var i = 1; i < classList.length; i++) {
+        if (classList[i].includes('-before')) classList.remove(classList[i]);
+      }
+    }, 400)
   }
 
   window.sendEmail = (e) => {
     e.preventDefault();
   }
+  // resize();
+  // window.addEventListener('resize', resize)
+
+  // function resize() {
+  //   const vw = window.innerWidth * 0.01
+  //   document.documentElement.style.setProperty('--vw', `${vw}`)
+  // }
 }
